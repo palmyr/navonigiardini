9692c1366584ddc01982be8c8678e591de9a890a
Ryan S Palm <rspalm@bigpond.com>
2021-03-13 09:51:19 +0100

    start work on page

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('scss', function(){
+    return gulp.src('./scss/import.scss')
+        .pipe(sass())
+        .pipe(concat('./style.css'))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('./scss/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('build', gulp.series('scss'));
